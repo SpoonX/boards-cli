d77d096f055da98a8a86fa73d2baa962251287c7
RWOverdijk <r.w.overdijk@gmail.com>
2017-10-12 11:36:10 +0200

    fix(project): allow nameless tasks to be run

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const {Runner}    = require('./lib/Runner');
 const config      = require(process.cwd() + '/boards.js');
 const task        = process.argv[2];
 const name        = process.argv[3];
-const pascalCased = name[0].toUpperCase() + name.substr(1);
-const upperCased  = name.replace(/([A-Z])/g, c => `_${c}`).toUpperCase();
+const pascalCased = name && name[0].toUpperCase() + name.substr(1);
+const upperCased  = name && name.replace(/([A-Z])/g, c => `_${c}`).toUpperCase();
 
 let runner = new Runner(config);
 
