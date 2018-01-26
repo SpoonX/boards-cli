b5da7e320fa0473398dd0e84b93b3cbccfbf8990
RWOverdijk <r.w.overdijk@gmail.com>
2018-01-26 14:42:22 +0100

    feat(project): add glob and default presets

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
 const ezon        = require('ezon');
+const fileExists  = require('file-exists');
 const {Runner}    = require('./lib/Runner');
-const config      = require(process.cwd() + '/boards.js');
 const task        = process.argv[2];
 const name        = process.argv[3];
 const pascalCased = name && name[0].toUpperCase() + name.substr(1);
 const upperCased  = name && name.replace(/([A-Z])/g, c => `_${c}`).toUpperCase();
+const boardsFile  = process.cwd() + '/boards.js';
+const config      = fileExists.sync(boardsFile) ? require(boardsFile) : {};
 
 let runner = new Runner(config);
 
