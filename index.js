90477f7e7e60db355f014d39367323270c84ef1c
RWOverdijk <r.w.overdijk@gmail.com>
2018-09-13 16:51:19 +0200

    feat(project): add cli param support

--- index.js
+++ index.js
@@ -10,8 +10,16 @@ const upperCased  = name && name.replace(/([A-Z])/g, c => `_${c}`).toUpperCase()
 const boardsFile  = process.cwd() + '/boards.js';
 const config      = fileExists.sync(boardsFile) ? require(boardsFile) : {};
 
+let parameters = require('minimist')(process.argv.slice(4));
+
+try {
+  parameters = Object.assign(parameters, ezon(process.argv[4]));
+} catch (e) {
+  // Well, this is awkward.
+}
+
 let runner = new Runner(config);
 
-runner.run(task, Object.assign(process.argv[4] ? ezon(process.argv[4]) : {}, {name, pascalCased, upperCased}))
+runner.run(task, Object.assign(parameters, {name, pascalCased, upperCased}))
   .then(() => console.log('Task completed.'))
   .catch(error => console.log('Task failed.', error));
